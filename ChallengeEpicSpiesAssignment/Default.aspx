06e7a80e821b0f87e1945f81f3dd37b37849b4e5
Becky Blue <becky.bluedodge@gmail.com>
2017-07-01 21:50:32 -0700

    Add files via upload

---
+++ ChallengeEpicSpiesAssignment/Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 199px;
+            height: 212px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img alt="spy" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+            <h2>Spy New Assignment Form</h2>
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="codeNameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            New Assignment Name:&nbsp; <asp:TextBox ID="newAssignmentTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            End Date of Previous Assignment:<asp:Calendar ID="previousCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Start Date of New Assignment:<asp:Calendar ID="newCalendar" runat="server"></asp:Calendar>
+            <br />
+            <br />
+            Projected End Date of New Assignment:<asp:Calendar ID="endCalendar" runat="server"></asp:Calendar>
+            <br />
+            <asp:Button ID="AssignButton" runat="server" OnClick="AssignButton_Click" Text="Assign Spy" />
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/Default.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                previousCalendar.SelectedDate = DateTime.Now.Date;
+                newCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                endCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+
+            }
+        }
+        protected void AssignButton_Click(object sender, EventArgs e)
+        {
+            TimeSpan totalDurationOfAssignment = endCalendar.SelectedDate.Subtract(newCalendar.SelectedDate);
+           
+            double totalCost = totalDurationOfAssignment.TotalDays * 500.0;
+            if (totalDurationOfAssignment.TotalDays > 21)
+            {
+                    totalCost += 1000.0;
+
+            }
+            
+            resultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Total budget: {2:C}",
+                codeNameTextBox.Text,
+                newAssignmentTextBox.Text,
+                totalCost);
+
+            TimeSpan timeBetweenAssignments = newCalendar.SelectedDate.Subtract(previousCalendar.SelectedDate);
+
+            if (timeBetweenAssignments.TotalDays < 14)
+            {
+                resultLabel.Text = "Error: Must allow 14 days between" + "last assignment and new assignment";
+                DateTime earliestNewAssignmentDate = previousCalendar.SelectedDate.AddDays(14);
+                newCalendar.SelectedDate = earliestNewAssignmentDate;
+                newCalendar.VisibleDate = earliestNewAssignmentDate;
+            }
+        }  
+                         
+
+                    
+    }
+}
